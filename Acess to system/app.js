8725a0c724bb200a0567cb8ebbf1f7242651f70f
danylollysychenko-ux <danylollysychenko@gmail.com>
2026-09-10 13:50:00 -0400

    finished with both of the assignments

---
+++ Acess to system/app.js
@@ -0,0 +1,40 @@
+const message = document.getElementById("displayMessage");
+const passwordInput = document.getElementById("password");
+const submitBtn = document.getElementById("submitPassword");
+const lockImage = document.getElementById("lockImage");
+const checkMarkImage = document.getElementById("checkMarkImage");
+let counter = 0;
+let attempts = 0;
+let password1 = "admin123";
+let password2 = "sigma67";
+let password3 = "system32";
+
+submitBtn.addEventListener("click", function () {
+  const show = document.getElementById(".show");
+  show?.classList.remove("show");
+  if (
+    passwordInput.value == password1 ||
+    passwordInput.value == password2 ||
+    passwordInput.value == password3
+  ) {
+    attempts = 0;
+    counter = 0;
+    message.innerHTML = "Acess Granted.";
+    checkMarkImage.classList.add("show")
+
+  } else {
+    message.innerHTML = "Invalid";
+    counter++;
+    if (counter == 3) {
+      attempts++;
+      counter = 0;
+      message.innerHTML = `Failed Attempt #${attempts}`;
+      if (attempts == 3) {
+        message.innerHTML = "MACHINE BLOCKED";
+        lockImage.classList.add("show");
+        submitBtn.disabled = true;
+        passwordInput.disabled = true;
+      }
+    }
+  }
+});
